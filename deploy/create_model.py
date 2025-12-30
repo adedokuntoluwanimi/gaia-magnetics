021103c263a33a070eb5ffdb9b2bc19e6e412a9e
Adedokun Toluwanimi <adedokuntoluwanimi@mtu.edu.ng>
2025-12-30 08:36:52 +0100

    please oh

--- deploy/create_model.py
+++ deploy/create_model.py
@@ -1,26 +1,20 @@
 import boto3
-from sagemaker import image_uris
 
 REGION = "us-east-1"
 ROLE_ARN = "arn:aws:iam::425933242610:role/gaia-ec2-s3-role"
 MODEL_NAME = "gaia-train-on-the-fly-model"
 
-
-
-IMAGE_URI = image_uris.retrieve(
-    framework="sklearn",
-    region=REGION,
-    version="1.2-1",
-    py_version="py3",
-    instance_type="ml.m5.large",
+# ✅ Correct, SageMaker-approved sklearn image for us-east-1
+IMAGE_URI = (
+    "683313688378.dkr.ecr.us-east-1.amazonaws.com/"
+    "sagemaker-scikit-learn:1.2-1-cpu-py3"
 )
-
 
 SOURCE_DIR_S3 = "s3://gaia22/ml/inference.tar.gz"
 
 sm = boto3.client("sagemaker", region_name=REGION)
 
-response = sm.create_model(
+sm.create_model(
     ModelName=MODEL_NAME,
     ExecutionRoleArn=ROLE_ARN,
     PrimaryContainer={
@@ -29,7 +23,6 @@ response = sm.create_model(
         "Environment": {
             "SAGEMAKER_PROGRAM": "inference.py",
             "SAGEMAKER_SUBMIT_DIRECTORY": SOURCE_DIR_S3,
-            "SAGEMAKER_CONTAINER_LOG_LEVEL": "20",
             "SAGEMAKER_REGION": REGION,
         },
     },
